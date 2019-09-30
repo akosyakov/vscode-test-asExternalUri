f3f4e0abddc204c004ca05bdfafd225579be32a7
Matt Bierner <matb@microsoft.com>
2019-09-30 13:38:33 -0700

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Test vs code extension for asExternalUri
+
+## Usage
+
+1. `npm i`
+1. `npm run compile`
+1. `f5`
+1. Run the `Test asExternalUri` command
---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+import * as vscode from 'vscode';
+import * as http from 'http';
+
+const PORT = 3000;
+
+export function activate(context: vscode.ExtensionContext) {
+
+  startServer(PORT);
+
+  const disposable = vscode.commands.registerCommand('test.asExternalUri', async () => {
+      const response = await vscode.window.showInputBox({
+        prompt: 'Enter uri to resolve',
+        placeHolder: 'http://localhost:3000'
+      });
+ 
+      if (!response) {
+        return
+      }
+
+      const resolved = vscode.env.asExternalUri(vscode.Uri.parse(response));
+      vscode.window.showInformationMessage(`Resolved to: ${resolved} on client and copied to clipboard`);
+      vscode.env.clipboard.writeText(resolved.toString());
+  });
+  context.subscriptions.push(disposable);
+}
+
+
+function startServer(port: number) {
+  const server = http.createServer((req, res) => {
+    res.end('Hello world!');
+  });
+  server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+  });
+  server.listen(port);
+  vscode.window.showInformationMessage(`Started local server on port: ${port}`);
+}
